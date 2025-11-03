690d3072e7db580ba968b859112460a8953a9006
projects-developer-dev <desenvolvimentoback@gmail.com>
2025-11-03 12:12:14 -0300

    Criação do ex014.

---
+++ exercicios/ex014/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+
+    var horaAtual = new Date().getHours();
+
+    horaAtual = 22
+
+    window.document.getElementById('msg').innerHTML = `Agora são ${horaAtual} horas`;
+
+     var img = window.document.getElementById('imagem');
+
+    if (horaAtual >= 0 && horaAtual < 12) {
+        img.src = '../ex014/imagens/fotomanha.png';
+        document.body.style.backgroundColor = '#fae9b3';
+    } else if (horaAtual >= 12 && horaAtual <= 18) {
+        img.src = '../ex014/imagens/fototarde.png'
+        document.body.style.backgroundColor = '#e18668';
+    } else {
+        img.src = '../ex014/imagens/fotonoite.png'
+        document.body.style.backgroundColor = '#2b2e2eff';
+    }
+}
